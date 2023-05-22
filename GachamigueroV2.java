b6ade75dc03b52a009e7901c400eddfad0f809af
Antonio Caparrós Hernández <antcapher@alu.edu.gva.es>
2023-05-22 17:56:50 +0200

    Redacto el programa de modo que pase los tests cuyo requerimiento sea Grachamiga

---
+++ GachamigueroV2.java
@@ -0,0 +1,17 @@
+public class GachamigaV2 {
+    public static void main(String[] args) {
+        GachamigaV2 p = new GachamigaV2();
+        p.start();
+    }
+
+    void start(){
+        for(int i=1;i<=100;i++)
+            System.out.println(resultadoGachamiga(i));
+    }
+
+    public static String resultadoGachamiga(int i){
+        if ((i%3==0)&&(i%5==0))
+            return "Gachamiga";
+        return "Nada";
+    }
+}
